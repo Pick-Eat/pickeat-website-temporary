95c1e2c79a6c9be20f4b332266a008ff667ad416
Eleonora Carletti <e.carletti@jetop.com>
2021-03-30 19:45:13 +0200

    copy again

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,9 @@ module.exports = {
         `gatsby-plugin-react-helmet`,
         `gatsby-source-data`,
         `gatsby-transformer-remark`,
+        `gatsby-plugin-advanced-sitemap`,
+        `gatsby-plugin-offline`,
+
         {
             resolve: `gatsby-source-filesystem`,
             options: {
@@ -28,6 +31,39 @@ module.exports = {
                 sourceUrlPath: `fields.url`,
                 pageContextProperty: `menus`,
             }
-        }
+        },
+        {
+            resolve: `gatsby-plugin-nprogress`,
+            options: {
+                // Setting a color is optional.
+                color: `blue`,
+                // Disable the loading spinner.
+                showSpinner: true,
+            },
+        },
+        {
+            resolve: 'gatsby-plugin-web-font-loader',
+            options: {
+                google: {
+                    families: ['Nunito Sans:400,400i,700,700i']
+                }
+            }
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `PickEat`,
+                short_name: `PickEat`,
+                start_url: `/`,
+                background_color: `#fefefe`,
+                theme_color: `#ff0000`,
+                display: `standalone`,
+                icon: `static/icon.png`,
+                icon_options: {
+                    // For all the options available, please see the additional resources below.
+                    purpose: `any maskable`,
+                },
+            },
+        },
     ]
 };
