42069d829125a64d8f07d32cc4a5f83cb65f29dc
Stackbit Code Editor <projects@stackbit.com>
2021-04-14 19:12:09 +0000

    SectionFeatures.js: changed

--- src/components/SectionFeatures.js
+++ src/components/SectionFeatures.js
@@ -37,14 +37,6 @@ export default class SectionFeatures extends React.Component {
                       {_.get(feature, 'actions', null) && (
                       <p className="block-buttons">
                         <CtaButtons {...this.props} actions={_.get(feature, 'actions', null)} />
-                      </p><div className="cell block-content">
-                      <h3 className="block-title underline">{_.get(feature, 'title', null)}</h3>
-                      <div className="block-copy">
-                        {markdownify(_.get(feature, 'content', null))}
-                      </div>
-                      {_.get(feature, 'actions', null) && (
-                      <p className="block-buttons">
-                        <CtaButtons {...this.props} actions={_.get(feature, 'actions', null)} />
                       </p>
                       )}
                     </div>
